38b3ec6df7cd7a3a86bf23fcf6bf6f7221380723
Alexey Zankevich <zancudero@gmail.com>
2012-09-09 18:46:00 +0300

    Hello message has been added to board

--- proto/uart/main.c
+++ proto/uart/main.c
@@ -1,55 +1,88 @@
- // this is the header file that tells the compiler what pins and ports, etc.
- // are available on this chip.
+//#define F_CPU 8000000UL  // 8 MHz
+
+/*Very Important - change F_CPU to match target clock 
+  Note: default AVR CLKSEL is 1MHz internal RC
+  This program transmits continously on USART. Interrupt is used for 
+	Receive charactor, which is then transmitted instead. LEDs are used 
+	as a test. RX routine is included but not used.
+  Change USART_BAUDRATE constant to change Baud Rate
+*/
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
- 
- // define what pins the LEDs are connected to.
- // in reality, PD6 is really just '6'
+
 #define LED PB0
- 
- // Some macros that make the code more readable
+
 #define output_low(port,pin) port &= ~(1<<pin)
 #define output_high(port,pin) port |= (1<<pin)
 #define set_input(portdir,pin) portdir &= ~(1<<pin)
 #define set_output(portdir,pin) portdir |= (1<<pin)
 
-void USART_Init( unsigned int baud )
-{
-  /* Set baud rate */
-  UBRRH = (unsigned char)(baud>>8);
-  UBRRL = (unsigned char)baud;
+// Define baud rate
+#define USART_BAUDRATE 9600   
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
-  /* Enable receiver and transmitter */
+volatile unsigned char value;  
+/* This variable is volatile so both main and RX interrupt can use it.
+It could also be a uint8_t type */
+
+/* Interrupt Service Routine for Receive Complete 
+NOTE: vector name changes with different AVRs see 
+Help - AVR-Libc reference - Library Reference - <avr/interrupt.h>: Interrupts
+for vector names other than USART_RXC_vect for ATmega32 */
+
+ISR(USART_RXC_vect){
+ 
+   value = UDR;             //read UART register into value
+   PORTB = ~value;          // output inverted value on LEDs (0=on)
+}
+
+void USART_Init(void){
+   // Set baud rate
+   UBRRL = BAUD_PRESCALE;// Load lower 8-bits into the low byte of the UBRR register
+   UBRRH = (BAUD_PRESCALE >> 8); 
+	 /* Load upper 8-bits into the high byte of the UBRR register
+    Default frame format is 8 data bits, no parity, 1 stop bit
+  to change use UCSRC*/ 
+
+  // Enable receiver and transmitter and receive complete interrupt 
   UCSRB = ((1<<TXEN)|(1<<RXEN) | (1<<RXCIE));
-  //UCSRB = (1<<RXEN)|(1<<TXEN);
-  /* Set frame format: 8data, 1stop bit */
-  //  UCSRC = (0<<USBS)|(3<<UCSZ0);
 }
 
-void USART_Transmit( unsigned int data )
-{
-  while ( !(UCSRA & (1<<UDRE)) ){
-    //nothing do
-  };
-  UDR = data;
+
+void USART_SendByte(uint8_t u8Data){
+
+  // Wait until last byte has been transmitted
+  while((UCSRA &(1<<UDRE)) == 0);
+
+  // Transmit data
+  UDR = u8Data;
 }
 
-int main(void) {
 
-  const unsigned int XTAL = 8000000;    
-  const unsigned int BAUDRATE = 9600;  
-  const unsigned int BAUDDIVIDER = XTAL/(16*BAUDRATE)-1;
-  USART_Init(BAUDDIVIDER);
+// not being used but here for completeness
+      // Wait until a byte has been received and return received data 
+uint8_t USART_ReceiveByte(){
+  while((UCSRA &(1<<RXC)) == 0);
+  return UDR;
+}
 
-  // initialize the direction of PORTB
-  set_output(DDRB, LED);  
-  while(1){
-    _delay_ms(50);
-    output_high(PORTB, LED); 
-    _delay_ms(1000);
-    output_low(PORTB, LED);
+int main(void){
+   USART_Init();  // Initialise USART
+   sei();         // enable all interrupts
+   set_output(DDRB, LED);
    
-    USART_Transmit('a');
-  }
+   for(;;){    // Repeat indefinitely
+     _delay_ms(1000);
+     USART_SendByte('H');
+     USART_SendByte('e');
+     USART_SendByte('l');
+     USART_SendByte('l');
+     USART_SendByte('o');
+     USART_SendByte(13);
+     output_high(DDRB, LED);
+     _delay_ms(200);
+     output_low(DDRB, LED);
+   }
 }
