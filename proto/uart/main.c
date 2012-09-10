5501196f596d44d43aa2723f57a257aca5b45808
Alexey Zankevich <zancudero@gmail.com>
2012-09-10 14:09:22 +0300

    Change initialization

--- proto/uart/main.c
+++ proto/uart/main.c
@@ -23,46 +23,19 @@
 #define USART_BAUDRATE 9600   
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
 
-volatile unsigned char value;  
-/* This variable is volatile so both main and RX interrupt can use it.
-It could also be a uint8_t type */
-
-/* Interrupt Service Routine for Receive Complete 
-NOTE: vector name changes with different AVRs see 
-Help - AVR-Libc reference - Library Reference - <avr/interrupt.h>: Interrupts
-for vector names other than USART_RXC_vect for ATmega32 */
-
-ISR(USART_RXC_vect){
- 
-   value = UDR;             //read UART register into value
-   PORTB = ~value;          // output inverted value on LEDs (0=on)
-}
-
 void USART_Init(void){
    // Set baud rate
-   UBRRL = BAUD_PRESCALE;// Load lower 8-bits into the low byte of the UBRR register
-   UBRRH = (BAUD_PRESCALE >> 8); 
-	 /* Load upper 8-bits into the high byte of the UBRR register
-    Default frame format is 8 data bits, no parity, 1 stop bit
-  to change use UCSRC*/ 
-
-  // Enable receiver and transmitter and receive complete interrupt 
+  UBRRL = BAUD_PRESCALE;
+  UBRRH = (BAUD_PRESCALE >> 8); 
   UCSRB = ((1<<TXEN)|(1<<RXEN) | (1<<RXCIE));
 }
 
 
 void USART_SendByte(uint8_t u8Data){
-
-  // Wait until last byte has been transmitted
   while((UCSRA &(1<<UDRE)) == 0);
-
-  // Transmit data
   UDR = u8Data;
 }
 
-
-// not being used but here for completeness
-      // Wait until a byte has been received and return received data 
 uint8_t USART_ReceiveByte(){
   while((UCSRA &(1<<RXC)) == 0);
   return UDR;
@@ -72,7 +45,7 @@ int main(void){
    USART_Init();  // Initialise USART
    sei();         // enable all interrupts
    set_output(DDRB, LED);
-   
+   output_low(DDRB, LED);
    for(;;){    // Repeat indefinitely
      _delay_ms(1000);
      USART_SendByte('H');
