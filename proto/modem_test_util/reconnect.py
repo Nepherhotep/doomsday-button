2e4b68a94befbd58446e6d4d6759b1c51e5b94fc
Alexey Zankevich <zancudero@gmail.com>
2012-09-10 21:45:44 +0300

    paths has changed

---
+++ proto/modem_test_util/reconnect.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Try to reconnect HID modem to last computer
+import serial
+import time
+
+
+def reconnect():
+    #setup and open serial
+    ser = serial.Serial()
+    ser.baudrate = 9600
+    ser.port = "/dev/tty.usbserial-AH019ML6"
+    ser.timeout = 5
+    ser.open()
+
+    #send disconnect command
+    ser.write('$$$')
+    print "Requesting command mode"
+    if ser.read(3) == "CMD":
+        print "Command mode enabled"
+
+    ser.write('C\r')
+    time.sleep(10)
+    
+    #close serial
+    print "Closing serial"
+    ser.close()
+
+
+if __name__ == '__main__':
+    reconnect()
